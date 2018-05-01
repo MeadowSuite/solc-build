0a3c90c83fb9f9383e6babe7e4281ac2a0aacc4c
Matt Little <zone117x@gmail.com>
2018-05-01 10:52:56 -0700

    Feature to specify windows lib build configuration

--- build-win.bat
+++ build-win.bat
@@ -13,8 +13,13 @@ if "%1" == "x86" (
     set cmake_gen="Visual Studio 15 2017"
 )
 
-set boost_dir="C:/local/boost_1_67_0"
-set boost_lib_dir="%boost_dir%/lib%arch%-msvc-14.1"
+set build_config=%2
+if "%2" == "" (
+    set build_config="RelWithDebInfo"
+)
+
+set boost_dir=C:/local/boost_1_67_0
+set boost_lib_dir=%boost_dir%/lib%arch%-msvc-14.1
 set boost_installer="boost_1_67_0-msvc-14.1-%arch%.exe"
 set boost_dl="https://iweb.dl.sourceforge.net/project/boost/boost-binaries/1.67.0/boost_1_67_0-msvc-14.1-%arch%.exe"
 
@@ -59,10 +64,10 @@ powershell -Command "(gc %solc_cmake%) -replace 'libsolc libsolc.cpp', 'libsolc 
 
 echo Creating cmake override to force static linking to runtime
 set cxx_flag_overrides="%source_dir%/cmake/cxx_flag_overrides.cmake"
-echo set(CMAKE_CXX_FLAGS_DEBUG_INIT          "/D_DEBUG /MTd /Zi /Ob0 /Od /RTC1") >> %cxx_flag_overrides%
-echo set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT     "/MT /O1 /Ob1 /D NDEBUG")           >> %cxx_flag_overrides%
-echo set(CMAKE_CXX_FLAGS_RELEASE_INIT        "/MT /O2 /Ob2 /D NDEBUG")           >> %cxx_flag_overrides%
-echo set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "/MT /Zi /O2 /Ob1 /D NDEBUG")       >> %cxx_flag_overrides%
+echo set(CMAKE_CXX_FLAGS_DEBUG_INIT          "/MT /Zi /Od /Ob0 /D NDEBUG")  >> %cxx_flag_overrides%
+echo set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT     "/MT /O1 /Ob1 /D NDEBUG")      >> %cxx_flag_overrides%
+echo set(CMAKE_CXX_FLAGS_RELEASE_INIT        "/MT /O2 /Ob2 /D NDEBUG")      >> %cxx_flag_overrides%
+echo set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "/MT /Zi /O2 /Ob1 /D NDEBUG")  >> %cxx_flag_overrides%
 
 echo Cmake generation for msvc solidity project
 cmake -G %cmake_gen% .. ^
@@ -72,13 +77,13 @@ cmake -G %cmake_gen% .. ^
     -DBoost_USE_MULTITHREADED=OFF ^
     -DBoost_USE_STATIC_LIBS=ON ^
     -DCMAKE_SUPPRESS_REGENERATION=TRUE ^
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
+    -DCMAKE_BUILD_TYPE=%build_config% ^
     -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
     -DCMAKE_USER_MAKE_RULES_OVERRIDE_CXX="%source_dir%/cmake/cxx_flag_overrides.cmake" ^
     || goto :error
 
 echo Preparing jsoncpp source
-msbuild jsoncpp-project.vcxproj /p:Configuration=RelWithDebInfo /m:4 /v:minimal
+msbuild jsoncpp-project.vcxproj /p:Configuration=%build_config% /m:4 /v:minimal
 
 echo Cmake generation for msvc jsoncpp project with static runtime linking
 cd "%source_dir%/build/deps/src/jsoncpp-project-build"
@@ -90,7 +95,7 @@ cmake -G %cmake_gen% "../jsoncpp-project" ^
 
 echo Building jsoncpp static lib
 msbuild "src/lib_json/jsoncpp_lib_static.vcxproj" ^
-    /p:Configuration=RelWithDebInfo /m:4 /v:minimal ^
+    /p:Configuration=%build_config% /m:4 /v:minimal ^
     /p:OutDir="%source_dir%/build/deps/lib/" ^
     /p:AssemblyName="jsoncpp" ^
     /p:TargetName="jsoncpp" ^
@@ -99,11 +104,11 @@ msbuild "src/lib_json/jsoncpp_lib_static.vcxproj" ^
 
 echo Building solidity solution
 cd "%source_dir%/build"
-msbuild solidity.sln /t:libsolc /p:Configuration=RelWithDebInfo /m:4 /v:minimal || goto :error
+msbuild solidity.sln /t:libsolc /p:Configuration=%build_config% /m:4 /v:minimal || goto :error
 
 echo Copying build files
 cd %start_dir%
-robocopy "%source_dir%/build/libsolc/RelWithDebInfo" "%start_dir%/build/win-%1" solc.dll
+robocopy "%source_dir%/build/libsolc/%build_config%" "%start_dir%/build/win-%1" solc.dll
 cd %start_dir%
 
 
