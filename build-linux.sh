6c126b0e1fa4757b8af476dd7ed0962a4d781788
Matt Little <zone117x@gmail.com>
2018-06-28 22:01:03 -0700

    Fix static linking against boost in linux build script - requires compiling boost

--- build-linux.sh
+++ build-linux.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+wget https://downloads.sourceforge.net/project/boost/boost/1.67.0/boost_1_67_0.tar.bz2 -O boost_1_67_0.tar.bz2
+tar --bzip2 -xf boost_1_67_0.tar.bz2
+cd boost_1_67_0
+./bootstrap.sh --with-libraries=regex,filesystem,program_options,system,test
+./b2 -j4 link=static cxxflags=-fPIC cflags=-fPIC
+cd ..
+
 cd solidity
 
 sed -i -e 's/DCMAKE_POSITION_INDEPENDENT_CODE=${BUILD_SHARED_LIBS}/DCMAKE_POSITION_INDEPENDENT_CODE=ON/g' cmake/jsoncpp.cmake
@@ -14,6 +21,8 @@ cmake .. \
     -DTESTS=Off \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-    -DBoost_USE_STATIC_LIBS=Off
+    -DBoost_USE_STATIC_LIBS=On \
+    -DBoost_NO_SYSTEM_PATHS=TRUE \
+    -DBOOST_ROOT=../boost_1_67_0
 
 make libsolc -j4
